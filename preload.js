979d1a63260540f713285643e90e8df2fd40af71
Dhruv Saini <56490747+oof-baroomf@users.noreply.github.com>
2025-05-27 21:03:27 -0700

    chore: remove electron app files and dependencies

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  spawnView: ({ wx, wy, url }) => ipcRenderer.invoke('spawn-browserview', { wx, wy, url }),
-  updateTransform: (pan, scale) => ipcRenderer.send('canvas-transform', { pan, scale }),
-  updateViewsZoomFactor: (scale) => ipcRenderer.send('update-views-zoom-factor', scale),
-  updateViewBounds:      ({ id, pos, size }) => ipcRenderer.send('update-view-bounds', { id, pos, size })
-});
--- titlebar-preload.js
+++
@@ -1,7 +0,0 @@
-const { ipcRenderer } = require('electron');
-
-// We'll add drag/resize handlers here in a future update
-// For now just make the titlebar interactive
-document.addEventListener('mousedown', e => {
-  e.preventDefault();
-});

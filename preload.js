e0c7aad772fe13f51beb23f479bb195eaba93d34
Dhruv Saini <56490747+oof-baroomf@users.noreply.github.com>
2025-05-31 21:13:23 -0700

    chore: Remove all project files

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    createView: (data) => ipcRenderer.invoke('create-view', data),
-    updateViewVisuals: (data) => ipcRenderer.send('update-view-visuals', data),
-    removeView: (id) => ipcRenderer.send('remove-view', id),
-    focusView: (id) => ipcRenderer.send('focus-view', id)
-});

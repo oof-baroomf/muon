e5665067c7c999852abf63c895db46de15c8fb6e
Dhruv Saini <56490747+oof-baroomf@users.noreply.github.com>
2025-06-04 20:37:04 -0700

    chore: remove electron app files and update gitignore

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    createView: (data) => ipcRenderer.send('create-view', data),
-    updateViewVisuals: (data) => ipcRenderer.send('update-view-visuals', data),
-    removeView: (id) => ipcRenderer.send('remove-view', id)
-});
